722e98446e46f5e9aa65b12415bd9eaada587985
Ishon Panda <pandaishon@gmail.com>
2026-07-03 19:52:36 +0530

    Finished Security - Adding Users

--- src/main/java/com/ecom/backend/model/Category.java
+++ src/main/java/com/ecom/backend/model/Category.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.util.List;
 
 @Entity(name = "categoryTable")
