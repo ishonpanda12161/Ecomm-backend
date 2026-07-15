cc54ca8a28c265f643996a391b8f2c5972741832
Ishon Panda <pandaishon@gmail.com>
2026-07-15 17:55:53 +0530

    v1 Monolth Done

--- src/main/java/com/ecom/backend/security/SecurityConfig.java
+++ src/main/java/com/ecom/backend/security/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
     {
         return httpSecurity.csrf(CsrfConfigurer::disable)
                 .authorizeHttpRequests(request ->
-                        request.requestMatchers("/api/open/**").permitAll()
+                        request.requestMatchers("/actuator/**").permitAll()
+                                .requestMatchers("/api/open/**").permitAll()
                                 .requestMatchers("/api/public/**").hasAnyRole("USER","ADMIN")
                                 .requestMatchers("/api/admin/**").hasAnyRole("ADMIN","SELLER")
                                 .anyRequest().authenticated())
